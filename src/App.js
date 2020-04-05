7cab27bda8453169eb1676961c4a1caf30f7cc19
H. Hizari <hhizari@gmail.com>
2020-04-05 22:49:45 +0700

    Setup project wide font

--- src/App.js
+++ src/App.js
@@ -1,43 +1,79 @@
 import React from 'react'
-import { StyleSheet, View, StatusBar } from 'react-native'
+import { StyleSheet, View, StatusBar, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import DropdownAlert from 'react-native-dropdownalert'
+import { useFonts } from '@use-expo/font'
 
 import NavigationScreen from './screens/NavigationScreen'
 import WebViewScreen from './screens/WebViewScreen'
 
 import AlertUtils from './utils/AlertUtils'
+import { AppLoading } from 'expo'
 
 const Stack = createStackNavigator()
 
+const Typography = () => {
+    const oldTextRender = Text.render
+    Text.render = function(...args) {
+		const origin = oldTextRender.call(this, ...args)
+		if (origin.props.style && origin.props.style.fontWeight) {
+			return React.cloneElement(origin, {
+				style: [origin.props.style, fontStyles.boldText],
+			})
+		} else {
+			return React.cloneElement(origin, {
+				style: [fontStyles.regularText, origin.props.style],
+			})
+		}
+    }
+}
+
+const fontStyles = StyleSheet.create({
+    regularText: {
+        fontFamily: "Muli",
+	},
+	boldText: {
+		fontFamily: "Muli-Bold",
+		fontWeight: "normal"
+    }
+}) 
+
 export default function App() {
-	return (
-		<View style={styles.container}>
+	const [regularLoaded] = useFonts({ Muli: require("./assets/fonts/Muli-Regular.ttf" )})
+	const [boldLoaded] = useFonts({ "Muli-Bold": require("./assets/fonts/Muli-Bold.ttf" )})
 
-			<NavigationContainer>
-				<Stack.Navigator screenOptions={{ headerShown: false }}>
-					<Stack.Screen 
-						name="Navigation" 
-						component={NavigationScreen}/>
-					<Stack.Screen 
-						name="WebView" 
-						component={WebViewScreen}/>
-				</Stack.Navigator>
-			</NavigationContainer>
-
-			<DropdownAlert
-				defaultContainer={{ marginTop: StatusBar.currentHeight ,padding: 8, flexDirection: 'row' }}
-				infoImageSrc={null}
-				warnImageSrc={null}
-				errorImageSrc={null}
-				successImageSrc={null}
-				ref={ref => AlertUtils.setDropDown(ref)}
-				onClose={() => AlertUtils.invokeOnClose()}
-			/>
-
-		</View>
-	)
+	if(!(regularLoaded && boldLoaded)){
+		return <AppLoading />
+	} else {
+		Typography()
+		return (
+			<View style={styles.container}>
+	
+				<NavigationContainer>
+					<Stack.Navigator screenOptions={{ headerShown: false }}>
+						<Stack.Screen 
+							name="Navigation" 
+							component={NavigationScreen}/>
+						<Stack.Screen 
+							name="WebView" 
+							component={WebViewScreen}/>
+					</Stack.Navigator>
+				</NavigationContainer>
+	
+				<DropdownAlert
+					defaultContainer={{ marginTop: StatusBar.currentHeight ,padding: 8, flexDirection: 'row' }}
+					infoImageSrc={null}
+					warnImageSrc={null}
+					errorImageSrc={null}
+					successImageSrc={null}
+					ref={ref => AlertUtils.setDropDown(ref)}
+					onClose={() => AlertUtils.invokeOnClose()}
+				/>
+	
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
